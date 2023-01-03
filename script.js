039fccf5996386213198b4c961758579bc0ca87b
Tolga <105448883+Tolga-Karabulut@users.noreply.github.com>
2023-01-03 22:40:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function discordalert(){
+    alert("Tolga#2965");
+    event.preventDefault();
+}
+function formalert() {
+    let formalert = confirm("Formu doğru doldurduğuna emin misin?");
+    if (formalert){
+      alert("Başvurunuz başarıyla gönderilmiştir!");
+    } 
+    else{
+      alert("Başvurunuz gönderilmemiştir lütfen bilgileri kontrol ediniz!");
+    }
+  }
+
+  
